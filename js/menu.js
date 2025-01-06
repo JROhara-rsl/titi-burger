675fd2d137ed8618d31c7c75e49177e2319f71fd
Julien Roussel <roussel-julien@outlook.fr>
2025-01-06 16:45:10 +0100

    Base menu JS et JSON

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("form-clickcollect");
+
+fetch('../json/menu.json')
+.then(response => response.json())
+.then(menus => { 
+    menus[0].categorie[0].burger[0].articles.forEach(burger => {
+
+        let menuDiv = document.createElement("div");
+        menuDiv.className = "produit";
+
+        menuDiv.innerHTML = `
+            <input type="radio" id="burger-${burger.id}" name="burger" value="${burger.id}" checked />
+            <div class="container-img">
+                <img src=".${burger.image}" class="card-img-top" alt="illustration ${burger.nom}">
+            </div>
+            <h2 class="border-bottom pb-2">${burger.nom}</h2>
+            <p>${burger.description}</p>
+        `;
+
+        container.appendChild(menuDiv);
+    });
+})
+
+.catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
