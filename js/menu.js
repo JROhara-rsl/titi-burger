e80094c5fb8baf0b05add312d976ed498e9a479e
Julien Roussel <roussel-julien@outlook.fr>
2025-01-07 16:49:07 +0100

    Debut menu js

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,84 @@
-const container = document.getElementById("form-clickcollect");
+// 1 - Récupérer les div de la page dans des constantes
+const buttonStart = document.getElementById("button-start");
+const containerIntro = document.getElementById("partie-clickcollect");
+const containerMenu = document.getElementById("partie-menu");
+const containerNav = document.getElementById("form-navigation");
+const containerBurger = document.getElementById("form-burger");
 
-fetch('../json/menu.json')
-.then(response => response.json())
-.then(menus => { 
-    menus[0].categorie[0].burger[0].articles.forEach(burger => {
 
-        let menuDiv = document.createElement("div");
-        menuDiv.className = "produit";
+// 2 - Préparer des bouttons next et before
+let buttonNext = document.createElement("button");
+buttonNext.setAttribute("id", "button-next");
+buttonNext.setAttribute("class", "button-menu");
+buttonNext.setAttribute("type", "button");
+buttonNext.innerHTML = `Suivant`;
 
-        menuDiv.innerHTML = `
-            <input type="radio" id="burger-${burger.id}" name="burger" value="${burger.id}" checked />
-            <div class="container-img">
-                <img src=".${burger.image}" class="card-img-top" alt="illustration ${burger.nom}">
-            </div>
-            <h2 class="border-bottom pb-2">${burger.nom}</h2>
-            <p>${burger.description}</p>
-            <div class="prix">${burger.price}€</div>
-        `;
 
-        container.appendChild(menuDiv);
-    });
-})
+// 3 - Lancer le programme lorsque l'on clique sur le boutton start
+buttonStart.addEventListener('click', function(event) {
+    containerIntro.innerHTML = "";
 
-.catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
+    containerMenu.classList.add("active");
+    
+    fetch('../json/menu.json')
+    .then(response => response.json())
+    .then(menus => { 
+        menus[0].categorie[0].burger[0].articles.forEach(burger => {
+
+            let menuDiv = document.createElement("div");
+            menuDiv.className = "produit";
+
+            menuDiv.innerHTML = `
+                <input type="radio" id="burger-${burger.id}" name="burger" value="${burger.id}" />
+                <div class="container-img">
+                    <img src=".${burger.image}" class="card-img-top" alt="illustration ${burger.nom}">
+                </div>
+                <h2 class="border-bottom pb-2">${burger.nom}</h2>
+                <p>${burger.description}</p>
+                <div class="prix">${burger.price}€</div>
+            `;
+
+            containerBurger.appendChild(menuDiv);
+        });
+    })
+    .catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
+
+    containerNav.appendChild(buttonNext);
+    buttonNext.setAttribute("onclick", "fonctionNext('accompagnement')");
+    setTimeout(() => {
+        containerBurger.setAttribute("style", "height: 1140px;");
+    }, 10);
+});
+
+// 4 - Fonction pour les boutons suivant
+
+
+function fonctionNext(identifiant){
+
+    fetch('../json/menu.json')
+    .then(response => response.json())
+    .then(menus => { 
+        const nomCategories = menus[0].categorie[0];
+        const nomCategorie = nomCategories[identifiant];
+        console.log(nomCategorie);
+
+        menus[0].categorie[0].nomCategorie[0].articles.forEach(produit => {
+            
+            let menuDiv = document.createElement("div");
+            menuDiv.className = "produit";
+
+            menuDiv.innerHTML = `
+                <input type="radio" id="burger-${produit.id}" name="burger" value="${produit.id}" />
+                <div class="container-img">
+                    <img src=".${produit.image}" class="card-img-top" alt="illustration ${produit.nom}">
+                </div>
+                <h2 class="border-bottom pb-2">${produit.nom}</h2>
+                <p>${produit.description}</p>
+                <div class="prix">${produit.price}€</div>
+            `;
+
+            containerBurger.appendChild(menuDiv);
+        });
+    })
+    .catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
+}
