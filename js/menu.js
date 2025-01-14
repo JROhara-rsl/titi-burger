6a5ff7388a6cee5f252dec857460c74c0418a768
Julien Roussel <roussel-julien@outlook.fr>
2025-01-14 13:51:05 +0100

    Nettoyage code

--- js/menu.js
+++ js/menu.js
@@ -31,72 +31,29 @@ const prixSelectBurger              = containerSelectBurger.querySelector('.prix
 const prixSelectAccompagnement      = containerSelectAccompagnement.querySelector('.prix');
 const prixSelectBoisson             = containerSelectBoisson.querySelector('.prix');
 const prixSelectDessert             = containerSelectDessert.querySelector('.prix');
-const divPrixTotal                     = document.querySelector('#prix-total');
-// 2 - Fonction pour abandonner
-function giveUp() {
-    buttonGiveup = document.getElementById("button-giveup");  
-    buttonGiveup.addEventListener("click", function(event){
-        event.preventDefault();
-        localStorage.clear();
-        maCommande = [];
-        document.location.href="../html/clickcollect.html";
-        console.log(maCommande);
-    })
-}
+const divPrixTotal                  = document.querySelector('#prix-total');
 
+// 2 - Fonction bouton commencer
+function fonctionStart() {
+    containerIntro.setAttribute("class","desactive");
+    containerStart.setAttribute("class","desactive");
+    containerIntroForm.setAttribute("style", "display: flex;")
 
-// 3 - Fonction pour les boutons suivant
-function fonctionNext(identifiant){
-    console.log(identifiant);
-    
-    // Créer une condition : il faut qu'un button radio soit sélectionné
-    // Seule exception, la première itération (-1)
-    if (identifiant > -1) {
-        let nomContainerBefore = "form-"+identifiant;
-        let containerBefore = document.getElementById(nomContainerBefore);
-        selectedBurger = containerBefore.querySelector('input:checked');
-    }
+    let container = document.getElementById('form-0');
+    container.classList.add('active');
 
-    if (identifiant == 3) {
-        document.location.href="../html/commande.html";
-    } else if (identifiant == -1 || selectedBurger) {
-        // Cacher la div d'intro et modifier le button
-        containerIntro.setAttribute("class","desactive");
-        containerStart.setAttribute("class","desactive");
-        containerInfo.classList.remove('active');
-        containerIntroForm.setAttribute("style", "display: flex;")
-
-        // Sélectionner et cacher la catégorie de menu précédente
-        if (identifiant > -1) {
-            let nomContainerCache = "form-"+(identifiant);
-            let containerCache = document.getElementById(nomContainerCache); 
-            containerCache.classList.remove('active');
-        }
-
-        // Sauvegarder les données dans le localStorage 
-        // Sélectioner le formulaire correspondant
-        let nomContainer = "form-"+identifiant;
-        let container = document.getElementById(nomContainer);
-
-        // Sélectionner le formualire correspondant à la catégorie de menu suivant
-        identifiant = parseInt(identifiant)+1;
-        nomContainer = "form-"+identifiant;
-        container = document.getElementById(nomContainer);
-        container.classList.add('active');
-
-        // Importer les articles de la bonne catégorie via la librairie JSON 
-        fetch('../json/menu.json')
+    fetch('../json/menu.json')
         .then(response => response.json())
         .then(menus => { 
             // Récupérer numéro et nom de la catégorie
-            let numCategorie = menus[0].categorie[identifiant];
-            let nomCategorie = menus[0].categorie[identifiant].nom;
+            let numCategorie = menus[0].categorie[0];
+            let nomCategorie = menus[0].categorie[0].nom;
 
             numCategorie.articles.forEach(produit => {   
                 let menuDiv = document.createElement("div");
                 menuDiv.className = "produit";
                 menuDiv.innerHTML = `
-                    <input type="radio" id="${nomCategorie}-${produit.id}" name="${nomCategorie}" value="${produit.id}" />
+                    <input type="radio" id="${nomCategorie}-${produit.id}" name="${nomCategorie}" value="${produit.nom}" />
                     <label id="label-${nomCategorie}-${produit.id}" for="${nomCategorie}-${produit.id}">
                         <div class="container-img">
                             <img src=".${produit.image}" class="card-img-top" alt="illustration ${produit.nom}">
@@ -112,23 +69,110 @@ function fonctionNext(identifiant){
         })
         .catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
 
-        // Générer bouton suivant
-        let navigationDiv = document.createElement("div");
-            navigationDiv.setAttribute("id", "partie-navigation");
-            navigationDiv.innerHTML = `
-                                <div class="container-navitation">
-                                    <div class="container-button">
-                                        <button id="button-giveup" class="button-menu">Abandonner</button>
-                                        <button id="button-next" class="button-menu" type="button" onClick="fonctionNext(${identifiant})">Suivant</button>
-                                    </div>
-                                </div>`                            
-        setTimeout(() => {
-            container.appendChild(navigationDiv);
-            giveUp()           
-        }, 100);
+        createButtonSuivant(0);
+}
+
+// 3 - Fonction pour les boutons suivant
+function fonctionNext(identifiant){    
+    // Créer une condition : il faut qu'un button radio soit sélectionné
+    // Seule exception, la première itération (-1)
+    if (identifiant > -1) {
+        let nomContainerBefore = "form-"+identifiant;
+        let containerBefore = document.getElementById(nomContainerBefore);
+        inputSelected = containerBefore.querySelector('input:checked');
+        containerSelection = document.getElementById("container-selection");
+    }
+
+    if (identifiant == 3) {
+        // Au bout de 3 itération sur l'identifiant, le menu est complet
+        // Donc on passe à la commande :
+        document.location.href="../html/commande.html";
+    } else if (inputSelected) {
+        containerInfo.classList.remove('active');
+
+        // Sélectionner et cacher la catégorie de menu précédente
+        let nomContainerCache = "form-"+(identifiant);
+        let containerCache = document.getElementById(nomContainerCache); 
+        containerCache.classList.remove('active');
+
+        // Sélectionner le formulaire correspondant à la catégorie de menu suivant
+        identifiant = parseInt(identifiant)+1;
+        nomContainer = "form-"+identifiant;
+        container = document.getElementById(nomContainer);
+        container.classList.add('active');
+
+        // Effet aggrandissement sur le container de sélection
+        /*const containerSelection = document.getElementById("container-selection");
+        const incrSelect = identifiant + 1;
+        console.log("incr="+incrSelect);
+        const containerSelect = containerSelection.childNodes[incrSelect];
+        console.log(containerSelect);
+        containerSelect.classList.add("active");*/
+
+        // Importer les articles de la bonne catégorie via la librairie JSON 
+        fetch('../json/menu.json')
+        .then(response => response.json())
+        .then(menus => { 
+            // Récupérer numéro et nom de la catégorie
+            let numCategorie = menus[0].categorie[identifiant];
+            let nomCategorie = menus[0].categorie[identifiant].nom;
+
+            numCategorie.articles.forEach(produit => {   
+                let menuDiv = document.createElement("div");
+                menuDiv.className = "produit";
+                menuDiv.innerHTML = `
+                    <input type="radio" id="${nomCategorie}-${produit.id}" name="${nomCategorie}" value="${produit.nom}" />
+                    <label id="label-${nomCategorie}-${produit.id}" for="${nomCategorie}-${produit.id}">
+                        <div class="container-img">
+                            <img src=".${produit.image}" class="card-img-top" alt="illustration ${produit.nom}">
+                        </div>
+                        <h2>${produit.nom}</h2>
+                        <p>${produit.description}</p>
+                        <div class="prix" value="${produit.price}">${produit.price}€</div>
+                    </label>
+                `;
+                container.appendChild(menuDiv);
+            });
+        })
+        .catch(error => console.log("Erreur lors du chargement du fichier JSON :", error));
+
+        createButtonSuivant(identifiant);
     } else {
         // Sinon, afficher la condition de sélectionner un bouton radio
         containerInfo = document.getElementById("form-information");
         containerInfo.setAttribute("class", "active");
     }
+}
+
+// 4 - Créer le bouton suivant 
+function createButtonSuivant(identifiant) {
+    let navigationDiv = document.createElement("div");
+    navigationDiv.setAttribute("id", "partie-navigation");
+    navigationDiv.innerHTML = `
+                                <div class="container-navitation">
+                                    <div class="container-button">
+                                        <button id="button-giveup" class="button-menu">Abandonner</button>
+                                        <button id="button-next" class="button-menu" type="button" onClick="fonctionNext(${identifiant})">Suivant</button>
+                                    </div>
+                                </div>`
+    setTimeout(() => {
+            // Récupérer l'identifiant du container
+            nomContainer = "form-"+identifiant;
+            container = document.getElementById(nomContainer);
+
+            container.appendChild(navigationDiv);
+            createButtonGiveUp()           
+        }, 100);                            
+}   
+
+// 5 - Créer un bouton abandonner
+function createButtonGiveUp() {
+    buttonGiveup = document.getElementById("button-giveup");  
+    buttonGiveup.addEventListener("click", function(event){
+        event.preventDefault();
+        localStorage.clear();
+        maCommande = [];
+        document.location.href="../html/clickcollect.html";
+        console.log(maCommande);
+    })
 }
