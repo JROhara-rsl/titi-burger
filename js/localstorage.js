85d36ab7d91cb093aaca0afd2d7bedd1719021d5
Julien Roussel <roussel-julien@outlook.fr>
2025-01-16 11:42:01 +0100

    Page pour payer

--- js/localstorage.js
+++ js/localstorage.js
@@ -93,11 +93,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
     majContainerSelection();
 });
-
-
-function numeroCommande() {
-    let min = Math.ceil(1000);
-    let max = Math.floor(9999);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-console.log(numeroCommande());
